8072f13f57af6bb8f9b57657bd605a9229562de6
vasuteotia <vteotia35@gmail.com>
2025-01-07 16:31:30 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Medical Project',
+    version= '0.0.0.0',
+    author= 'Vasu Teotia',
+    author_email= 'vteotia35@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
